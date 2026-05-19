7e8a3e97cce0f59320c22640cbba874c22fec8ac
Daniel <qertyuipadfghjjlzcvbm@gmail.com>
2026-05-19 14:30:55 +1000

    Make search input styled

--- scripts/make_header.py
+++ scripts/make_header.py
@@ -20,6 +20,10 @@ def make_header():
     with d.div(cls="search bold"):
         with d.button(cls="search-button"):
             du.raw(
-                '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>'
+                s='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>'
             )
-        d.input_(type="search", placeholder="Search books & more")
+        d.input_(
+            type="search",
+            placeholder="Search books & more",
+            id="search-input",
+        )
