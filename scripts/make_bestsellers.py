779029e8c7ba442db4715fc0cce6dc202cbeb00e
Daniel <qertyuipadfghjjlzcvbm@gmail.com>
2026-05-19 16:33:21 +1000

    Add bestsellers

--- scripts/make_bestsellers.py
+++ scripts/make_bestsellers.py
@@ -1,2 +1,20 @@
+import dominate.tags as d
+import yaml
+from paths import cover_path
+
+
 def make_bestsellers():
-    pass
+    books = yaml.full_load(open("sources/books.yaml", "r"))
+    bestsellers = yaml.full_load(open("sources/bestsellers.yaml", "r"))
+
+    for key in bestsellers:
+        book = books[key]
+
+        with d.div(cls="bestseller"):
+            d.img(
+                src=cover_path(key),
+                alt=f"Cover of {book['title']}",
+            )
+            with d.div(cls="bestseller-details"):
+                d.h3(book["title"])
+                d.address(book["author"])
