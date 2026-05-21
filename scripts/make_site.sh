cd28d9584417d459d378e2a48cf7f0924c3e6e33
Daniel <101683475+Koranir@users.noreply.github.com>
2026-05-22 01:26:48 +1000

    Add author and titles pages

--- scripts/make_site.sh
+++ scripts/make_site.sh
@@ -2,18 +2,25 @@
 
 mkdir -p site
 mkdir -p site/products
+mkdir -p site/authors
 mkdir -p site/data
 
 uv run scripts/make_home.py >site/index.html
 uv run scripts/make_products.py
+uv run scripts/make_titles.py >site/titles.html
+uv run scripts/make_authors.py >site/authors.html
+uv run scripts/make_author_profiles.py
 uv run scripts/make_cart.py >site/cart.html
 uv run scripts/make_cart_products.py >site/data/cart-products.json
 uv run scripts/make_order_confirmation.py >site/order-confirmation.html
 
 prettier -w site/index.html
 prettier -w site/products/*.html
+prettier -w site/titles.html
+prettier -w site/authors.html
+prettier -w site/authors/*.html
 prettier -w site/data/*.json
 
-ln -rs ./assets ./site/
-ln -rs ./style ./site/
-ln -rs ./sources/js ./site/
+ln -rsfn ./assets ./site/assets
+ln -rsfn ./style ./site/style
+ln -rsfn ./sources/js ./site/js
