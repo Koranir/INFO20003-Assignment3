f3107461ff09121e2bae52d536ff76df6986b214
Daniel <101683475+Koranir@users.noreply.github.com>
2026-06-07 23:23:07 +1000

    Fix section titles in deeper nesting

--- scripts/make_section_title.py
+++ scripts/make_section_title.py
@@ -1,11 +1,10 @@
 import dominate.tags as d
-from paths import asset_path
 
 
 def make_section_title(text, asset=None):
     attrs = {"cls": "section-title"}
 
     if asset:
-        attrs["style"] = f"--styled-asset-path: url('../{asset_path(asset)}')"
+        attrs["style"] = f"--styled-asset-path: url('../assets/{asset}')"
 
     return d.h2(text, **attrs)
